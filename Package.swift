2e60a4fd9e17b3b845a95bd21f40b9bce653573e
Aya <ayabaghdadi83@yahoo.com>
2021-07-27 21:02:29 +0200

    package

--- Package.swift
+++ Package.swift
@@ -1,8 +1,22 @@
-//
-//  Package.swift
-//  MyProject
-//
-//  Created by OSX on 27/07/2021.
-//
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import Foundation
+import PackageDescription
+
+let package = Package(
+    name: "MyProject",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "MyProject",
+            dependencies: []),
+        .testTarget(
+            name: "MyProjectTests",
+            dependencies: ["MyProject"]),
+    ]
+)
